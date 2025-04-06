fe7e6aace12ff951fab4881e5bb3fd46febc2f2f
Aliaksandr Martsul <sasham9321@mail.ru>
2025-04-06 20:38:57 +0300

    change server link

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
-// export const API_BASE_URL =
-//     "https://questionnaire-server-cwqc.onrender.com/api";
-export const API_BASE_URL = "http://localhost:10000/api";
+export const API_BASE_URL =
+    "https://questionnaire-server-cwqc.onrender.com/api";
+// export const API_BASE_URL = "http://localhost:10000/api";
 
 export const endpoints = {
     signin: `/signin`,
