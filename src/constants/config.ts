6dfa39b0b85f1b1d9f8f1bf65dfe2d791b31b12e
Aliaksandr Martsul <sasham9321@mail.ru>
2025-04-29 11:43:44 +0300

    links

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,9 +1,9 @@
 export const API_BASE_URL =
-    "https://questionnaire-server-cwqc.onrender.com/api";
+    "https://questionnaire-server-gajm.onrender.com/api";
 // export const API_BASE_URL = "http://localhost:10000/api";
 
 export const WS_BASE_URL =
-    "wss://questionnaire-server-cwqc.onrender.com/api/comments?formId=";
+    "wss://questionnaire-server-gajm.onrender.com/api/comments?formId=";
 // export const WS_BASE_URL = "ws://localhost:10000/api/comments?formId=";
 
 export const endpoints = {
