c2dbe9dc023a85bdbe64f1ba2691154c00e4fc1d
martsul <sasham9321@mail.ru>
2025-05-17 12:05:56 +0300

    loader index

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -2,13 +2,14 @@ import { Modal, Spinner } from "react-bootstrap";
 import { useLoading } from "../../contexts/loading-context/use-loading";
 
 export const Loader = () => {
-    const {isLoading} = useLoading()
+    const { isLoading } = useLoading();
 
     return (
         <Modal
+            style={{ zIndex: 1060 }}
             show={isLoading}
             contentClassName="bg-transparent border-0"
-            className="d-flex align-items-center justify-content-center"
+            className={"d-flex align-items-center justify-content-center"}
         >
             <Spinner animation="border" />
         </Modal>
